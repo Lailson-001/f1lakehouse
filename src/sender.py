ecf2ded99f2a68d801eb27a631b09abc433e993e
Lailson Santos <santoslailson90581@gmail.com>
2026-06-26 21:25:39 -0300

    fix: correção de bug em sender.py.

--- src/sender.py
+++ src/sender.py
@@ -1,24 +1,28 @@
-#%%
-import dotenv
 import os
-import boto3
-from tqdm import tqdm
 import argparse
+from pathlib import Path
 
-#%%
+import boto3
+import dotenv
+from tqdm import tqdm
+
 dotenv.load_dotenv()
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
 
-#%%
 AWS_KEY = os.getenv("AWS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 
+if not AWS_KEY or not AWS_SECRET_KEY:
+    raise ValueError("Credenciais AWS não encontradas no arquivo .env")
 
-#%%
+
 class Sender:
-    def __init__(self, bucket_name, bucket_folder):
+    def __init__(self, bucket_name, bucket_folder="results"):
         self.bucket_name = bucket_name
-        self.bucket_folder = bucket_folder
+        self.bucket_folder = bucket_folder.rstrip("/")
+
         self.s3 = boto3.client(
             "s3",
             aws_access_key_id=AWS_KEY,
@@ -27,45 +31,73 @@ class Sender:
         )
 
     def process_file(self, filename):
-        file = filename.split("/")[-1]
-        bucket_path = os.path.join(self.bucket_folder, file)
+        file_name = Path(filename).name
+        bucket_path = f"{self.bucket_folder}/{file_name}"
 
         try:
             self.s3.upload_file(
-                filename,
-                self.bucket_name,
-                bucket_path,
+                Filename=filename,
+                Bucket=self.bucket_name,
+                Key=bucket_path,
             )
 
+            print(f"Upload realizado: {bucket_path}")
+
         except Exception as err:
-            print(err)
+            print(f"Erro ao enviar {file_name}: {err}")
             return False
 
         os.remove(filename)
         return True
-    
-    
+
     def process_folder(self, folder):
+        if not os.path.isdir(folder):
+            print(f"Pasta '{folder}' não existe.")
+            return
+
         files = os.listdir(folder)
-        for i in tqdm(files) :
-            self.process_file(os.path.join(folder, i))
-                                
-#%%
+
+        for file in tqdm(files):
+            filepath = os.path.join(folder, file)
+
+            if os.path.isfile(filepath):
+                self.process_file(filepath)
+
+
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--bucket", type=str)
-    parser.add_argument("--bucket_path", default="f1/results", type=str)
-    parser.add_argument("--folder", default="data", type=str)
+
+    parser.add_argument(
+        "--bucket",
+        required=True,
+        help="Nome do bucket S3",
+    )
+
+    parser.add_argument(
+        "--bucket_path",
+        default="results",
+        help="Pasta dentro do bucket",
+    )
+
+    parser.add_argument(
+        "--folder",
+        default=str(DATA_DIR),
+        help="Pasta local com os arquivos",
+    )
+
     args = parser.parse_args()
-    
-    if args.bucket:
-        send = Sender(args.bucket, args.bucket_path)  
-        send.process_folder(args.folder) 
-    else:
-        print("sem bucket definido")                       
 
+    sender = Sender(
+        bucket_name=args.bucket,
+        bucket_folder=args.bucket_path,)
 
+    sender.process_folder(args.folder)
+
+parser.add_argument(
+    "--bucket_path",
+    default="f1-results-raw/results",
+    type=str,
+)
         
 
 
